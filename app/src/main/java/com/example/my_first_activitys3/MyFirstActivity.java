de878dae8d1aeb0b9e4d5998da5dcd27fad3be85
Esmeralda <velazquezesmeraldard@gmail.com>
2023-06-17 12:47:36 -0600

    Activity Uno y Activity Dos

--- app/src/main/java/com/example/my_first_activitys3/MyFirstActivity.java
+++ app/src/main/java/com/example/my_first_activitys3/MyFirstActivity.java
@@ -3,6 +3,8 @@ package com.example.my_first_activitys3;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Toast;
+import android.content.Intent;
+import android.net.Uri;
 
 public class MyFirstActivity extends Activity {
     @Override
@@ -14,7 +16,13 @@ public class MyFirstActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this, "¡Bienvenido de vuelta!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "¡Primer Activity!", Toast.LENGTH_SHORT).show();
+
+        //Intent intent = new Intent(Intent.ACTION_VIEW);
+        //intent.setData(Uri.parse("https://es-la.facebook.com"));
+        //startActivity(intent);
+        Intent intent = new Intent(this, MySecondActivity.class);
+        startActivity(intent);
     }
 
 }
