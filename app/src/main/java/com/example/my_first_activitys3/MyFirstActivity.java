6b0a2c65ea8ab7d430b3b7543b095fb11aa22b64
Esmeralda <velazquezesmeraldard@gmail.com>
2023-06-17 11:46:32 -0600

    Mensaje inicial

--- app/src/main/java/com/example/my_first_activitys3/MyFirstActivity.java
+++ app/src/main/java/com/example/my_first_activitys3/MyFirstActivity.java
@@ -1,4 +1,20 @@
 package com.example.my_first_activitys3;
 
-public class MyFirstActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MyFirstActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitylayoutfirst);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "¡Bienvenido de vuelta!", Toast.LENGTH_SHORT).show();
+    }
+
 }
